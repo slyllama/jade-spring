c89845c5c9a48975176049f1700167833f36f069
Alec <alex.laing@me.com>
2024-11-23 13:56:53 +1300

    #21 improved main menu nodule behavior
    And ingested title card animation assets

--- lib/main_menu/main_menu.gd
+++ lib/main_menu/main_menu.gd
@@ -2,29 +2,32 @@ extends CanvasLayer
 
 const LOADER_SCENE = "res://lib/loader/loader.tscn"
 
-@onready var nodule_pos = Vector2.ZERO
+@onready var focus: Button
 
 # Connections and tweens to make the focus nodule look right
 func set_up_nodule() -> void:
 	$Nodule.modulate.a = 0.0
 	
 	$Container/PlayButton.focus_entered.connect(func():
-		nodule_pos = $Container/PlayButton.global_position)
+		focus = $Container/PlayButton)
 	$Container/SettingsButton.focus_entered.connect(func():
-		nodule_pos = $Container/SettingsButton.global_position)
+		focus = $Container/SettingsButton)
 	$Container/QuitButton.focus_entered.connect(func():
-		nodule_pos = $Container/QuitButton.global_position)
+		focus = $Container/QuitButton)
 	
 	$Container/PlayButton.mouse_entered.connect(func():
-		nodule_pos = $Container/PlayButton.global_position)
+		focus = $Container/PlayButton)
 	$Container/SettingsButton.mouse_entered.connect(func():
-		nodule_pos = $Container/SettingsButton.global_position)
+		focus = $Container/SettingsButton)
 	$Container/QuitButton.mouse_entered.connect(func():
-		nodule_pos = $Container/QuitButton.global_position)
+		focus = $Container/QuitButton)
 	
 	await get_tree().create_timer(0.55).timeout
 	var _f = create_tween()
 	_f.tween_property($Nodule, "modulate:a", 1.0, 0.2)
+
+func _set_title_card_pos() -> void:
+	$TitleCard.global_position = $Container/PlayButton.global_position + Vector2(240, -140)
 
 func _ready() -> void:
 	set_up_nodule()
@@ -39,12 +42,18 @@ func _ready() -> void:
 				else: get_window().mode = Window.MODE_WINDOWED
 	)
 	
+	get_window().size_changed.connect(_set_title_card_pos)
+	
+	
 	SettingsHandler.refresh(["volume"])
 	await get_tree().process_frame
+	_set_title_card_pos()
 	$Container/PlayButton.grab_focus()
 
 func _process(delta: float) -> void:
-	$Nodule.global_position = lerp($Nodule.position, nodule_pos + Vector2(-24, 16), delta * 22)
+	if focus == null: return
+	$Nodule.global_position = lerp(
+		$Nodule.position, focus.global_position + Vector2(-24, 16), delta * 22)
 
 func _on_play_button_down() -> void:
 	get_tree().change_scene_to_file(LOADER_SCENE)
---
+++ lib/main_menu/title_card/title_card.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var active = false
+
+# Set shader values on a scale of 0-1 with easings
+func _set_val(val: float, node: Sprite2D) -> void:
+	if active: return
+	var _mat = node.material
+	_mat.set_shader_parameter("paint_mask_exponent", 0.1 + (1 - ease(val, 0.25)) * 9.9)
+	_mat.get_shader_parameter("paint_mask_gradient").fill_to.x = val
+	_mat.get_shader_parameter("paint_mask_gradient").fill_from.x = val * 0.5 + 0.5
+
+func _ready() -> void:
+	# Duplicate shaders so that their functionality is independent
+	# Set initial visibilities and shader configurations
+	$Words1.material = $Words1.material.duplicate(true)
+	$Words2.material = $Words2.material.duplicate(true)
+	$Words1.visible = false
+	$Words2.visible = false
+	_set_val(0.0, $Words1)
+	_set_val(0.0, $Words2)
+	
+	# Start delay
+	$StartDelay.start()
+	await $StartDelay.timeout
+	$Words1.visible = true
+	$Words2.visible = true
+	
+	# Handle animations
+	var words_1_tween = create_tween()
+	words_1_tween.tween_method(
+		_set_val.bind($Words1), 0.0, 0.99, 0.9).set_ease(Tween.EASE_OUT)
+	await get_tree().create_timer(0.25).timeout
+	var words_2_tween = create_tween()
+	words_2_tween.tween_method(
+		_set_val.bind($Words2), 0.0, 0.99, 0.94).set_ease(Tween.EASE_OUT)
