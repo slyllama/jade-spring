3aec341f22c2fea5b736b969bf4231afa6e6e3ae
slyllama <alex.laing@me.com>
2024-12-11 15:51:28 +1300

    Weed-clearing effects

--- lib/crumb/weed.gd
+++ lib/crumb/weed.gd
@@ -1,10 +1,14 @@
 @tool
 extends Crumb
 
+var rng = RandomNumberGenerator.new()
+var pickable = true
+
 func _ready() -> void:
 	super()
+	$Foam.emitting = false
 	body_entered.connect(func(body):
-		if body is CharacterBody3D:
+		if pickable and body is CharacterBody3D:
 			Global.weed_crumb_entered.emit())
 	
 	body_exited.connect(func(body):
@@ -12,6 +16,11 @@ func _ready() -> void:
 			Global.weed_crumb_left.emit())
 
 func interact() -> void:
+	if !pickable: return
+	pickable = false
 	Global.add_qty_effect("weed")
-	Global.announcement_sent.emit("((Weed picked))")
-	queue_free()
+	#Global.announcement_sent.emit("((Weed picked))")
+	var _pitch = rng.randf_range(0.9, 1.1)
+	$LeafSound.pitch_scale = _pitch
+	$LeafSound.play()
+	$Player.play("clear")
