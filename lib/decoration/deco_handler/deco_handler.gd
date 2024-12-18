30892e91e2a6ec8ca7dd8c56b3ebe87379f7e057
Alex Laing <alex.laing@me.com>
2024-12-19 01:17:44 +1300

    Prevent bamboo decorations from casting shadows

--- lib/decoration/deco_handler/deco_handler.gd
+++ lib/decoration/deco_handler/deco_handler.gd
@@ -66,7 +66,7 @@ func _get_decoration_list() -> Array:
 func _save_decorations() -> void:
 	var _decoration_save_data = _get_decoration_list()
 	for _n in _decoration_save_data:
-		print("saving " + _n.id + " w rotation.x " + str(_n.rotation.x))
+		print("Saving decoration'" + _n.id + "' with rotation.x " + str(_n.rotation.x))
 	var _file = FileAccess.open(FILE_PATH, FileAccess.WRITE)
 	_file.store_var(_decoration_save_data)
 	_file.close()
