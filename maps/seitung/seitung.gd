59a9b0fc27dd003be7410237edc4d331ea990bb3
Alec <alex.laing@me.com>
2025-01-03 09:20:46 +1300

    JADE-31 merge landscape changes

--- maps/seitung/seitung.gd
+++ maps/seitung/seitung.gd
@@ -1,42 +1,44 @@
 extends "res://lib/map/map.gd"
 
-const Dialogue = preload("res://lib/dialogue/dialogue.tscn")
+@onready var toolbox = get_node("HUD/Toolbox")
+var y_target = 0.0
+
+#func spawn_spider() -> void:
+	#var _s = load("res://lib/spider/spider.tscn").instantiate()
+	#add_child(_s)
+	#_s.global_position = Vector3(
+		#Global.player_position.x,
+		#y_target,
+		#Global.player_position.z)
+	#
+	#Global.move_player.emit(Vector3(
+		#Global.player_position.x,
+		#y_target + 1.0,
+		#Global.player_position.z))
+	#
+	#Global.walk_mode_target = _s
 
 func set_marker_pos() -> void: # set the position of the story marker
 	match Save.data.story_point:
-		"game_start": $StoryMarker.position = $Decoration/Foliage/Pulley.position
+		"game_start": $StoryMarker.position = $Pulley.position
 		"pick_weeds": $StoryMarker.position = $WeedBin.position
-		"clear_bugs": $StoryMarker.position = $Discombobulator.position + Vector3(-0.5, 0, 0)
+		"clear_bugs": $StoryMarker.global_position = $Discombobulator/SpatialText.global_position - Vector3(0, 0.9, 0)
 		"_": $StoryMarker.position = Vector3(0, -10, 0) # hide under map
 
 func _ready() -> void:
+	#Global.walk_mode_target = $Spider
+	
+	$Landscape/LandscapeCol.set_collision_layer_value(2, true)
 	super()
 	
 	Save.story_advanced.connect(set_marker_pos)
 	await get_tree().process_frame
 	set_marker_pos()
 	
-	$DragonvoidArc/AnimationPlayer.play("Wobble")
-	$DragonvoidArc2/AnimationPlayer.play("Wobble")
-	$DragonvoidArc3/AnimationPlayer.play("Wobble")
+	#Global.walk_mode_entered.connect(spawn_spider)
+	#Global.walk_mode_left.connect(func():
+		#Global.walk_mode_target.queue_free())
+	#Global.command_sent.emit("/cleardeco")
 
-func _on_discombobulator_interacted() -> void:
-	Global.add_effect.emit("discombobulator")
-
-func _on_bin_interacted() -> void:
-	if Global.get_effect_qty("weed") > 0:
-		$WeedBin/Foam.emitting = true
-		$WeedBin/LeafSound.play()
-		await get_tree().create_timer(0.3).timeout
-		$WeedBin/Foam.emitting = false
-	else: # do weed stuff
-		Global.announcement_sent.emit("((Comment about no weeds))")
-	Save.data.deposited_weeds += Global.get_effect_qty("weed")
-	Global.crumbs_updated.emit()
-	Save.data.weeds = 0
-	Global.remove_effect.emit("weed")
-
-func _on_dialogue_test_interacted() -> void:
-	var _d = Dialogue.instantiate()
-	add_child(_d)
-	_d.open()
+#func _process(_delta: float) -> void:
+	#y_target = $Player/YCast.get_collision_point().y
