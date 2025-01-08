ebdb450d1dc0611e83e804568dc5552d73163021
slyllama <alex.laing@me.com>
2025-01-08 15:25:00 +1300

    JADE-10 Dragonvoid screen FX

--- lib/crumb/bug_swarm.gd
+++ lib/crumb/bug_swarm.gd
@@ -11,7 +11,7 @@ func clear() -> void:
 func _ready() -> void:
 	super()
 	body_entered.connect(func(body):
-		if Save.data.story_point == "game_start": return # not unlocked yet
+		#if Save.data.story_point == "game_start": return # not unlocked yet
 		if body is CharacterBody3D:
 			if !$BugEntry.playing:
 				$BugEntry.pitch_scale = 0.9 + rng.randf() * 0.2
@@ -20,12 +20,14 @@ func _ready() -> void:
 			Global.bug_crumb_entered.emit())
 	
 	body_exited.connect(func(body):
-		if Save.data.story_point == "game_start": return # not unlocked yet
+		#if Save.data.story_point == "game_start": return # not unlocked yet
 		if body is CharacterBody3D:
 			Global.bug_crumb_left.emit())
 
 func interact() -> void:
-	if Save.data.story_point == "game_start": return # not unlocked yet
+	if Save.data.story_point == "game_start":
+		Global.announcement_sent.emit("((Comment about bugs))")
+		return # not unlocked yet
 	
 	if "discombobulator" in Global.current_effects:
 		var _f = FishingInstance.instantiate()
--- lib/hud/screen_effects/screen_effects.gd
+++ lib/hud/screen_effects/screen_effects.gd
@@ -3,16 +3,42 @@ extends CanvasLayer
 # Illustrate states like being in Dragonvoid, around bugs, etc
 
 var bug_state = false
+var dv_state = false
 var bug_tween: Tween
+var dv_tween: Tween
 
 func _set_bugs_exponent(val) -> void:
 	var _e = ease(val, 2.0)
 	_e = 0.8 + 9.2 * _e
 	$Bugs.material.set_shader_parameter("alpha_exponent", _e)
 
+func _set_dragonvoid_exponent(val) -> void:
+	var _e = ease(val, 2.0)
+	_e = 0.8 + 9.2 * _e
+	$Dragonvoid.material.set_shader_parameter("alpha_exponent", _e)
+
 func _ready() -> void:
 	_set_bugs_exponent(10.0)
+	_set_dragonvoid_exponent(10.0)
 	$Bugs.visible = false
+	$Dragonvoid.visible = false
+	
+	Global.dragonvoid_crumb_entered.connect(func():
+		dv_state = true
+		$Dragonvoid.visible = true
+		dv_tween = create_tween()
+		dv_tween.tween_method(
+			_set_dragonvoid_exponent, 1.0, 0.0, 0.15))
+	
+	Global.dragonvoid_crumb_left.connect(func():
+		dv_state = false
+		$Dragonvoid.visible = true
+		dv_tween = create_tween()
+		dv_tween.tween_method(
+			_set_dragonvoid_exponent, 0.0, 1.0, 0.15)
+		
+		dv_tween.tween_callback(func():
+			if !dv_state: $Dragonvoid.visible = false))
 	
 	Global.bug_crumb_entered.connect(func():
 		bug_state = true
