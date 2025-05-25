8c5b32789f21839c5692dece33e3c04f02f3db0c
Alec <alex.laing@me.com>
2025-05-25 13:18:37 +1200

    Added version check

--- lib/hud/version.gd
+++ lib/hud/version.gd
@@ -2,12 +2,24 @@ extends RichTextLabel
 # Version
 # Display the game's version number
 
+const Flash = preload("res://lib/flash/flash.tscn")
 @export var align_right = false
 
-func _display_version() -> void:
+func _display_version(newer_version: String = Version.VER) -> void:
 	var _str = "v" + Version.VER
+	if newer_version != Version.VER:
+		_str += " [color=yellow](v" + newer_version + " available)[/color]"
+		var _f = Flash.instantiate()
+		if align_right:
+			_f.position.x += size.x
+		add_child(_f)
 	if align_right: text = "[right]" + _str + "[/right]"
 	else: text = _str
 
 func _ready() -> void:
+	gui_input.connect(func(_event):
+		if Input.is_action_just_pressed("left_click"):
+			OS.shell_open("https://slyllama.net/jade-spring"))
+	Version.latest_version_retrieved.connect(func(latest_version):
+		_display_version(latest_version))
 	_display_version()
--- version.gd
+++ version.gd
@@ -2,3 +2,17 @@ extends Node
 
 const VER = "1.0b"
 const TARGET = "itch"
+const VER_HTTPS = "https://www.slyllama.net/jade-spring/vercheck.json"
+
+signal latest_version_retrieved(latest_version)
+@onready var request = HTTPRequest.new()
+
+func _ready():
+	add_child(request)
+	request.request_completed.connect(func(result, status, _headers, body):
+		if result != 0: return # couldn't connect
+		if status != HTTPClient.RESPONSE_OK: return # invalid connection
+		var json_data = JSON.parse_string(body.get_string_from_utf8())
+		if "versions" in json_data:
+			latest_version_retrieved.emit(json_data.versions[0]))
+	request.request(VER_HTTPS)
