f816b64b74a53de2a29b74ab7b41455cea34e6e1
Alec <alex.laing@me.com>
2025-07-18 00:22:14 +1200

    Linked data to decoration cards

--- lib/decoration/decoration_pane/featured_pane/deco_card/deco_card.gd
+++ lib/decoration/decoration_pane/featured_pane/deco_card/deco_card.gd
@@ -1,10 +1,20 @@
+@tool
 extends Panel
+
+@export var title: String = "((Untitled))"
+@export var id: String = ""
+@export var image: Texture2D
+@export_tool_button("Update Preview") var update_button = update
+
+func update() -> void:
+	$Title.text = title
+	$Image.texture = image
 
 func _init() -> void:
 	modulate.a = 0.9
 
-func _on_mouse_entered() -> void:
-	modulate.a = 1.2
+func _ready() -> void:
+	update()
 
-func _on_mouse_exited() -> void:
-	modulate.a = 0.9
+func _on_mouse_entered() -> void: modulate.a = 1.2
+func _on_mouse_exited() -> void: modulate.a = 0.9
--- lib/player/salmon/salmon.gd
+++ lib/player/salmon/salmon.gd
@@ -2,4 +2,6 @@
 extends Node3D
 
 func _ready() -> void:
+	$SalmonMesh/AnimationPlayer.animation_finished.connect(func(_anim):
+		$SalmonMesh/AnimationPlayer.play("flop"))
 	$SalmonMesh/AnimationPlayer.play("flop")
